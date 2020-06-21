16fbe44de6ec3e875040762aa4bbdf2f26ded03d
Arash Keighobadi <arashkeyghobadi@gmail.com>
2020-06-21 11:26:44 +0200

    client-server communication stablished

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host:       'localhost',
+    user:       'root',
+    password:   'root',
+    database:   'todoit'
+})
+
+db.connect((err) => {
+    if(err) throw err;
+    console.log('MySql connected...');
+})
+
+const app = express();
+
+//testing the connection
+app.get('/connect', (req, res) => {
+    // if(err) throw err;
+    res.send('You reached the server!');
+})
+
+//create a database
+app.get('/createdb', (req, res, err) => {
+    if(err) throw err;
+
+    let sql = 'CREATE DATABASE todoit'
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created.');
+    })
+})
+
+//create todos table
+app.get('/create-todos-table', (req, res) => {
+    let sql = 'CREATE TABLE todos(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL, completed BOOLEAN)'
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        console.log(result);
+        res.send('todos table created.');
+    })
+})
+
+//select all records from the todos table
+app.get('/get-todos', (req, res) => {
+    let sql = 'SELECT * FROM todos'
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        console.log(result);
+        res.send('todos fetched.');
+    })
+})
+
+//select a single record
+app.get('/get-todo/:id', (req, res) => {
+    let sql = 'SELECT * FROM todos WHERE id = ' + connection.escape(req.params.id)
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        console.log(result);
+        res.send('todo with id '+ connection.escape(req.params.id) +' fetched.');
+    })
+})
+
+app.get('/delete-post/:id', (req, res) => {
+    let sql = 'DELETE * FROM todos WHERE id = ' + connection.escape(req.params.id)
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        console.log(result);
+        res.send('todo with id '+ connection.escape(req.params.id) +' deleted.');
+    })
+})
+
+
+
+app.listen('3000', () => {
+    console.log('server started on port 3000...');
+})
